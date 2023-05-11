05a1295b80771d64133f50aea8936ffec34de36a
Jacob Lewallen <jlewallen@gmail.com>
2023-05-11 15:45:42 -0700

    Process proto into rust code using prost.

--- libs/query/src/lib.rs
+++ libs/query/src/lib.rs
@@ -1,14 +1,16 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub struct Client {}
+
+impl Client {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
+
+pub mod http {
+    include!(concat!(env!("OUT_DIR"), "/fk_app.rs"));
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    // use super::*;
 }
